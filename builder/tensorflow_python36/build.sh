e155ca77f759b4f064b541d396e5174e3d7bf931
Martin Svedin <svedi@kth.se>
2017-04-19 22:26:34 +0200

    Streamlining building tensorflow from source

---
+++ builder/tensorflow_python36/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./configure
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
